f8527ab9f3e23271a51b111692e64871c635c267
Prachi Tripathi <95475712+prachitripathi22@users.noreply.github.com>
2023-07-18 21:04:25 +0530

    Added solution - LeetHub

---
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+	public:
+		int LongestRepeatingSubsequence(string str){
+		    // Code here
+		    int n =str.length();
+		    vector<vector<int>> ans(n+1,vector<int>(n+1,0));
+		    for(int i=1;i<=n;i++)
+		    {
+		        for(int j=1;j<=n;j++)
+		        {
+		   if (str[i-1]==str[j-1] && i!=j)
+		            {
+		                ans[i][j] = ans[i-1][j-1] +1;
+		            }
+		            else
+		            {
+		                ans[i][j]=max(ans[i-1][j],ans[i][j-1]);
+		            }
+		        }
+		    }
+		    return ans[n][n];
+		    
+		}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string str;
+		cin >> str;
+		Solution obj;
+		int ans = obj.LongestRepeatingSubsequence(str);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
